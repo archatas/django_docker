2a950ed27da287dbee0ca52eb1576015493286c3
Aidas Bendoraitis <aidasbend@yahoo.com>
2019-07-22 06:06:39 +0200

    ⚙️ Apps in INSTALLED_APPS grouped by source.

--- src/myproject/myproject/settings/_base.py
+++ src/myproject/myproject/settings/_base.py
@@ -44,12 +44,17 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    # contributed
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party
+    # ...
+    # local
+    # ...
 ]
 
 MIDDLEWARE = [
